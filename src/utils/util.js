c89860d6e878ee21807454c4c285e4abeeed4014
zhiyul <liuzhiyu17@qq.com>
2016-05-04 13:46:44 +0800

    window->_window

--- src/utils/util.js
+++ src/utils/util.js
@@ -15,17 +15,15 @@
             return div.innerText;
         },
         observeDOM: (function(){
-            var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-                eventListenerSupported = window.addEventListener;
+            var MutationObserver = _window.MutationObserver || _window.WebKitMutationObserver,
+                eventListenerSupported = _window.addEventListener;
 
             return function(obj, callback){
                 if( MutationObserver ){
-                    // define a new observer
                     var obs = new MutationObserver(function(mutations, observer){
                         if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
                             callback();
                     });
-                    // have the observer observe foo for changes in children
                     obs.observe( obj, { childList:true, subtree:true });
                 }
                 else if( eventListenerSupported ){
